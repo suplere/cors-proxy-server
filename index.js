ffe12d73724e0494c9ba1551c53513c43aa6274e
Evžen Šupler <evzen.supler@gmail.com>
2020-05-22 11:20:45 +0200

    Add console log

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const request = require('request');
-const multer  = require('multer')
+const multer = require('multer')
 const upload = multer()
 
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
 });
 
 app.get('/www.fio.cz/ib_api/rest/*.json', (req, res) => {
     request(
-        { 
-            url: `https:/${req.path}` 
+        {
+            url: `https:/${req.path}`
         },
         (error, response, body) => {
             if (error || response.statusCode !== 200) {
                 return res.status(500).json({ type: 'error', message: error.message });
             }
-        res.json(JSON.parse(body));
+            res.json(JSON.parse(body));
         }
     )
 });
 
-app.post('/www.fio.cz/ib_api/rest/import/',upload.single('file'), (req, res) => {
+app.post('/www.fio.cz/ib_api/rest/import/', upload.single('file'), (req, res) => {
     const file = req.file
     // console.log(file)
     const formData = {
@@ -32,23 +32,23 @@ app.post('/www.fio.cz/ib_api/rest/import/',upload.single('file'), (req, res) => 
         file: {
             value: file.buffer,
             options: {
-              filename: 'davka.xml',
-              contentType: 'text/xml'
+                filename: 'davka.xml',
+                contentType: 'text/xml'
             }
         }
     }
     request.post(
-        { 
+        {
             url: `https:/${req.path}`,
-            formData: formData 
+            formData: formData
         },
         (error, response, body) => {
             if (error || response.statusCode !== 200) {
-                // console.log(response.body)
+                console.log(response)
                 return res.status(response.statusCode).json({ type: 'error', message: error && error.message ? error.message : '' });
-              }
-              res.send(body);
-        })    
+            }
+            res.send(body);
+        })
 })
 
 const PORT = process.env.PORT || 3000;
